911068d954e29a82aaf70e22948f839911818baf
Teddy-Hung <tedhungkk@gmail.com>
2020-12-29 19:56:13 -0800

    done

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const getProducts = require('./getProducts')
+const getProduct = require('./getProduct')
+
+const app = express()
+
+const port = 3001
+
+app.get('/api/products', getProducts)
+app.get('/api/products/:id', getProducts)
+app.listen(port, () => console.log(`Server listening on Port: ${port}`))
+
